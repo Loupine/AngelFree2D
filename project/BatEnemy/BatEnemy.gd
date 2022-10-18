d341b22866643fc88c3a602f9dd168d4c942c7b9
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-10-18 02:30:16 -0400

    Delete bat when off screen and add to enemies group

--- project/BatEnemy/BatEnemy.gd
+++ project/BatEnemy/BatEnemy.gd
@@ -6,10 +6,13 @@ var _velocity := Vector2.ZERO
 
 func _ready():
 	randomize()
-	position.x = 1300
-	position.y = rand_range(60, 660)
+	position.x = 1930
+	position.y = rand_range(0, 1000)
 
 
 func _physics_process(_delta):
 	_velocity.x = -200
 	_velocity = move_and_slide(_velocity, Vector2.UP)
+	
+	if position.x < -100:
+		queue_free()
