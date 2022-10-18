daddcef39d39cb4d27fd497a0e1a2b955946e426
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-10-18 02:54:11 -0400

    Refactor code

--- project/BatEnemy/BatEnemy.gd
+++ project/BatEnemy/BatEnemy.gd
@@ -4,13 +4,13 @@ extends KinematicBody2D
 var _velocity := Vector2.ZERO
 
 
-func _ready():
+func _ready()-> void:
 	randomize()
 	position.x = 1930
 	position.y = rand_range(0, 1000)
 
 
-func _physics_process(_delta):
+func _physics_process(_delta)-> void:
 	_velocity.x = -200
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 	
--- project/World/World.gd
+++ project/World/World.gd
@@ -4,16 +4,16 @@ extends Control
 onready var bat_enemy_preload := preload("res://BatEnemy/BatEnemy.tscn")
 
 
-func _ready():
+func _ready()-> void:
 	randomize()
 
 
-func _spawn_enemies(var count : int):
+func _spawn_enemies(var count : int)-> void:
 	for enemy in count:
 		var new_bat := bat_enemy_preload.instance()
 		add_child(new_bat)
 
 
-func _on_Timer_timeout():
-	randomize()
+func _on_Timer_timeout()-> void:
 	_spawn_enemies(randi() % 5)
+	randomize()
