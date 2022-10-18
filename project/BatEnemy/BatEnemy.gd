88de3a8341151e0cfb85de55842cc19c3d2b470e
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-10-18 03:10:12 -0400

    Set random speed and only collide with player

--- project/BatEnemy/BatEnemy.gd
+++ project/BatEnemy/BatEnemy.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 
+var _speed := 0.0
 var _velocity := Vector2.ZERO
 
 
@@ -8,10 +9,11 @@ func _ready()-> void:
 	randomize()
 	position.x = 1930
 	position.y = rand_range(0, 1000)
+	_speed = rand_range(-800, -200)
 
 
 func _physics_process(_delta)-> void:
-	_velocity.x = -200
+	_velocity.x = _speed
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 	
 	if position.x < -100:
