3d1af5c5a9ffd89dc2e77228b840c0040a58a792
Andrew Everage-Scheible <88517712+Loupine@users.noreply.github.com>
2022-10-20 03:26:31 -0400

    Add functional restart button

--- project/World/World.gd
+++ project/World/World.gd
@@ -7,6 +7,7 @@ var _score := 0
 onready var bat_enemy_preload := preload("res://BatEnemy/BatEnemy.tscn")
 onready var player := get_node("Player")
 onready var score_label := get_node("ScoreLabel")
+onready var restart_button := get_node("RestartButton")
 
 
 func _ready()-> void:
@@ -15,8 +16,19 @@ func _ready()-> void:
 
 func _physics_process(_delta)-> void:
 	if _is_player_alive:
-		_score += 1
-		score_label.text = String(_score)
+		_increment_score()
+	
+
+
+func _increment_score():
+	_score += 1
+	score_label.text = String(_score)
+
+
+func _show_restart_button():
+	restart_button.rect_position.x = 710
+	restart_button.rect_position.y = 440
+	restart_button.text = "Final score: %s. Restart?" % String(_score)
 
 
 func _spawn_enemies(var count : int)-> void:
@@ -32,3 +44,9 @@ func _on_SpawnTimer_timeout()-> void:
 
 func _on_Player_player_died():
 	_is_player_alive = false
+	_show_restart_button()
+
+
+func _on_RestartButton_pressed():
+	# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
